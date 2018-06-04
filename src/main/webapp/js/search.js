f36c440cafeca3cc5eac873907cdfaff6ff14850
bugtamer <javrm357@gmail.com>
2018-06-04 13:13:34 +0200

    FIX no permitir busquedas vacias

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,24 +1,26 @@
-(function () {
+function buscar(event) {
+	event.preventDefault();
+	console.log(`on submit() >>> $('#icon_prefix2').val()='${$('#icon_prefix2').val()}'`);
 	const SEARCH_FORM = document.getElementById('search_form');
-	SEARCH_FORM.onsubmit = function (event) {
-		event.preventDefault();
-	    if ($('#icon_prefix2').val() == "") {
-	        $('#sugerencias').html('<p class="error">El campo de búsqueda no puede estar vacío</p>');
-	    } else {
-	    	SEARCH_FORM.submit();
-	    }
-	}
-	
-	
+	if ($('#icon_prefix2').val() == "") {
+        $('#sugerencias').html('<p class="error">El campo de búsqueda no puede estar vacío</p>');
+    } else {
+    	SEARCH_FORM.submit();
+    }
+}
+
+
+
+(function () {
 	document.getElementById('search_button').onclick = function (evnt) {
-	    SEARCH_FORM.submit();
+		buscar(evnt);
 	};
 	
 	
 	document.getElementById('icon_prefix2').onkeypress = function (e) {
 	    const ENTER_KEY_CODE = 13;
 	    if (e.keyCode == ENTER_KEY_CODE) {
-		    SEARCH_FORM.submit();
-	    }
+			buscar(e);
+		}
 	};
 })();
