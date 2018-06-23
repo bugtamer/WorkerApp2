8cecb6648fb5c80d9e0a05aecf710c4ae0546f5b
Unknown <35902230+bugtamer@users.noreply.github.com>
2018-06-23 12:49:33 +0200

    DELETE javascripts sin uso

--- src/main/webapp/js/redirect.js
+++
@@ -1,9 +0,0 @@
-var count = 2;
-setInterval(function(){
-    count--;
-    document.getElementById('timecount');
-    if (count == 0) {
-        url = sessionStorage.getItem('ultimaPaginaVisitada');
-        window.location = url ? url : './index.jsp'; // por si el usuario visita directamente login.jsp sin otra página previa
-    }
-},1000);
