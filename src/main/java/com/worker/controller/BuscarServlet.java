e09c3168eaa17215045dacb8f907fa434685a611
Unknown <javrm357@gmail.com>
2018-07-18 20:16:23 +0200

    FIX excepción al mostrar resultados de búsqueda

--- src/main/java/com/worker/controller/BuscarServlet.java
+++ src/main/java/com/worker/controller/BuscarServlet.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.worker.db.DDBB;
 import com.worker.models.Manitas;
+import com.worker.persistence.ManitasEM;
 import com.worker.util.UbicacionHelper;
 
 
@@ -19,7 +19,7 @@ public class BuscarServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		System.out.println("BuscarServlet - doGet()");
 
 		if (request.getAttribute("sonResultados") == null) {
@@ -32,19 +32,18 @@ public class BuscarServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		System.out.println("BuscarServlet - doPost()");
-		
+
 		UbicacionHelper.setUbicacion(request);
 		String busqueda= request.getParameter("terminosDeBusqueda");
-		DDBB db = DDBB.getInstance();
-		List<Manitas> resultado = db.getResultadosBusqueda(busqueda);
+		List<Manitas> resultado = ManitasEM.getInstance().getListaByProfession(busqueda);
 		request.setAttribute("sonResultados", true);
 		request.setAttribute("listaManitas", resultado);
 		request.setAttribute("terminoBusqueda", busqueda);
-		
+
 		System.out.println("terminosDeBusqueda = " + busqueda);
-		
+
 		doGet(request, response);
 	}
 
