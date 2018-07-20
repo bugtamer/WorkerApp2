123a8a257d6c3332b6a05495da898354e59bf717
Unknown <javrm357@gmail.com>
2018-07-20 09:50:25 +0200

    API REST put, post y delete profesional por id (post sin id)

--- src/main/java/com/worker/services/ProfesionalService.java
+++ src/main/java/com/worker/services/ProfesionalService.java
@@ -1,14 +1,15 @@
 package com.worker.services;
 
-import javax.ws.rs.PathParam;
-
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,7 +17,7 @@ import javax.ws.rs.core.Response.Status;
 
 import com.worker.models.Manitas;
 import com.worker.models.Ubicacion;
-import com.worker.models.Usuario;
+import com.worker.models.Valoracion;
 import com.worker.persistence.ManitasEM;
 import com.worker.persistence.dao.DAO;
 import com.worker.persistence.dao.UbicacionDAO;
@@ -29,24 +30,40 @@ import com.worker.persistence.dao.manitas.ValoracionDAO;
 @Path("/prof")
 public class ProfesionalService {
 
-	public static Manitas unManitas;
+//	public static Manitas unManitas;
 
 	@PUT
 	@Path("/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response actualizarProfesional(@PathParam(value = "id") int idp, Manitas man) {
+	public Response actualizarProfesional(@PathParam(value = "id") int idp, Manitas man,
+			@HeaderParam("token") String token) {
+		
+		// ¿AUTORIZADO?
+		AuthService auth = new AuthService();
+		int userTokenId = auth.getUsuarioIdFromToken(token);
+		if(userTokenId == DAO.NO_ID) {
+			return Response.status(Status.FORBIDDEN).build();
+		}
+		// PROCEDE:
+		System.out.println("put id=" + idp + ", man=" + man);
+		Response response = null;
 		boolean isOk = false;
 		if (man.getId() == idp) {
 			try {
 				isOk = ManitasEM.getInstance().actualizar(man);
+				if (isOk) {
+					response =  Response.status(202).entity(isOk).build();
+				} else {
+					response =  Response.status(404).entity(isOk).build();
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			return Response.status(202).entity(isOk).build();
 		} else {
-			return Response.status(Status.FORBIDDEN.getStatusCode()).entity("Error").build();
+			response = Response.status(Status.FORBIDDEN.getStatusCode()).entity("Error").build();
 		}
+		return response;
 	}
 
 	
@@ -54,7 +71,16 @@ public class ProfesionalService {
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@DELETE
-	public Response deleteProfesional(@PathParam(value = "id") int idp) {
+	public Response deleteProfesional(@PathParam(value = "id") int idp,
+			@HeaderParam("token") String token) {
+		
+		// ¿AUTORIZADO?
+		AuthService auth = new AuthService();
+		int userTokenId = auth.getUsuarioIdFromToken(token);
+		if(userTokenId == DAO.NO_ID) {
+			return Response.status(Status.FORBIDDEN).build();
+		}
+		// PROCEDE:
 		System.out.println(String.format("deleteProfesional(%d)", idp));
 		Response res = null;
 		// FIXME deberia utilizar una transaccion para esto
@@ -67,17 +93,21 @@ public class ProfesionalService {
 			int ubiId = DAO.NO_ID;
 			Map<String, Object> usuMap = UsuarioDAO.getInstance().read(idp);
 			if (usuMap != null) {
-				ubiId = (int) usuMap.get("ubi_id");
+				ubiId = (usuMap.get("ubi_id") == null) ? DAO.NO_ID : (int) usuMap.get("ubi_id");
 			}
 			
 			boolean usuFueBorrada = UsuarioDAO.getInstance().delete(idp);
 			boolean ubiFueBorrada = UbicacionDAO.getInstance().delete(ubiId);
 			
-			if(expFueBorrada && eduFueBorrada && valFueBorrada &&
-					proFueBorrada && usuFueBorrada && ubiFueBorrada ) {
-				System.out.println(String.format("deleteProfesional(%d) = 202", idp));
-				res = Response.status(202).build();
-				System.out.println(String.format("deleteProfesional(%d)2 = 202", idp));
+			if (proFueBorrada) {
+				if(expFueBorrada && eduFueBorrada && valFueBorrada &&
+						usuFueBorrada && ubiFueBorrada ) {
+					System.out.println(String.format("deleteProfesional(%d) = 202", idp));
+					res = Response.status(202).build();
+					System.out.println(String.format("deleteProfesional(%d)2 = 202", idp));
+				}
+			} else {
+				res = Response.status(404).entity("").build();
 			}
 		} catch (Exception e) {
 			System.out.println(String.format("deleteProfesional(%d) = 500", idp));
@@ -93,17 +123,50 @@ public class ProfesionalService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@POST
-	public Response newProfesional(Manitas man) {
-
+	public Response newProfesional(Manitas man,
+			@HeaderParam("token") String token) {
+		
+		// ¿AUTORIZADO?
+		AuthService auth = new AuthService();
+		int userTokenId = auth.getUsuarioIdFromToken(token);
+		if(userTokenId == DAO.NO_ID) {
+			return Response.status(Status.FORBIDDEN).build();
+		}
+		// PROCEDE:
+		System.out.println("POST man = " + man);
 		Response res = null;
 		try {
 			Ubicacion ubi = man.getUbicacion();
 			int ubiID = UbicacionDAO.getInstance().create(ubi.getLatitud(), ubi.getLongitud());
 			int usuID = UsuarioDAO.getInstance().create(man.getNombre(), man.getApellidos(), man.getEmail(),
-					man.getPassword(), null, man.getId());
-			int profID = ProfesionDAO.getInstance().create(man.getId(), man.getProfesion());
-			int expID = ExperienciaDAO.getInstance().create(man.getId(), null);
-			int eduID = EducacionDAO.getInstance().create(man.getId(), null);
+					man.getPassword(), man.getAvatar(), ubiID);
+			int profID = ProfesionDAO.getInstance().create(usuID, man.getProfesion());
+			
+			List<String> expList = man.getExperiencia();
+			if ((expList != null) && (expList.size() > 0)) {
+				for (String exp : expList) {
+					ExperienciaDAO.getInstance().create(profID, exp);
+				}
+			}
+			
+			List<String> eduList = man.getEducacion();
+			if ((eduList != null) && (eduList.size() > 0)) {
+				for (String edu : eduList) {
+					EducacionDAO.getInstance().create(profID, edu);
+				}
+			}
+			
+			List<Valoracion> valList = man.getValoraciones();
+			if ((valList != null) && (valList.size() > 0)) {
+				for (Valoracion val : valList) {
+					ValoracionDAO.getInstance().create(
+							val.getComentario(),
+							val.getPuntuacion(),
+							val.getAutor().getId(),
+							profID);
+				}
+			}
+			
 			res = Response.status(202).entity(usuID).build();
 		} catch (Exception e) {
 			res = Response.status(500).entity(e.getMessage()).build();
