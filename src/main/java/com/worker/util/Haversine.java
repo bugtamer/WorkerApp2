d0ef1111e0ccb551121f2f3bd6284bd79e1526fe
bugtamer <javrm357@gmail.com>
2018-07-22 16:49:33 +0200

    FIX cálculo de las distancias

--- src/main/java/com/worker/util/Haversine.java
+++ src/main/java/com/worker/util/Haversine.java
@@ -9,17 +9,17 @@ public class Haversine {
     public static final double EARTH_RADIUS_R_KM = 6372.795477598;
     
     public static double calcDistantKm(double lat1, double lon1, double lat2, double lon2) {
-        lat1 = Math.toRadians(lat1);
-        lon1 = Math.toRadians(lon1);
-        lat2 = Math.toRadians(lat2);
-        lon2 = Math.toRadians(lon2);
-        return EARTH_RADIUS_R_KM * Math.acos(
-        		Math.sin(lat1) *
-        		Math.sin(lat2) +
-        		Math.cos(lat1) *
-        		Math.cos(lat2) *
-        		Math.cos(lat1-lat2)
-        	);
+        double radLat1 = lat2 * Math.PI/180;
+        double radLat2 = lat1 * Math.PI/180;
+        double theta = lon2 - lon1;
+        double radTheta = theta * Math.PI/180;
+        double dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+        dist = (dist > 1) ? 1 : dist;
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344; // parse to km
+        return dist;
     }
     
 }
--- src/main/webapp/WEB-INF/tags/distancia.tag
+++ src/main/webapp/WEB-INF/tags/distancia.tag
@@ -1,4 +1,7 @@
 <%@ attribute name="ubicacion1" required="true" type="com.worker.models.Ubicacion" %>
 <%@ attribute name="ubicacion2" required="true" type="com.worker.models.Ubicacion" %>
 
-<h5>${(ubicacion1 != null) && (ubicacion2 != null) ? String.format("%,.1fm", 1000 * ubicacion1.getDistanciaKilometrica( ubicacion2 )) : "Geo no habilitada"}</h5>
+<h5>
+	${(ubicacion1 != null) && (ubicacion2 != null) ? String.format("%,.1f km", ubicacion1.getDistanciaKilometrica( ubicacion2 )) : "Geo no habilitada"}
+	<i class="material-icons">${(ubicacion1 != null) && (ubicacion2 != null) ? "location_on" : "location_off"}</i>
+</h5>
